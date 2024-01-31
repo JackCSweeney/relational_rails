f2f695a5f7047a878ebc92045b0e9d1004531884
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-30 16:39:04 -0800

    feat: Add ability to look at individual Restaurant page with all attributes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/restaurants", to: "restaurants#index"
+  get "/restaurants/:id", to: "restaurants#show"
 
   # Defines the root path route ("/")
   # root "posts#index"
