623c78f76f996865dbba78e6802b3856890f1114
Jack Sweeney <jackcsweeney@gmail.com>
2024-02-06 13:07:09 -0800

    refactor: Refactor find_cooks method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,7 @@
 class Restaurant < ApplicationRecord
     has_many :cooks
     def find_cooks
-        Cook.all.find_all do |cook|
-            cook.restaurant_id == self.id
-        end
+        Cook.where("restaurant_id = #{self.id}")
     end
 
     def cook_count
